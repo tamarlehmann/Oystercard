469cab478941d74e3d5f4d2992020f57e9c6d720
Sam Mckay <sammckay12@gmail.com>
2017-01-09 18:19:40 +0000

    Refactored max bal error

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,22 +12,11 @@ describe Oystercard do
     expect(card.top_up(10)).to eq card.balance
   end
 
-<<<<<<< HEAD
   it 'throws error if balance exceeds 90' do
     card = Oystercard.new
     maximum_bal = Oystercard::MAX_BALANCE
     expect{ card.top_up(91) }.to raise_error("Balance cannot exceed #{maximum_bal}")
-=======
-  it 'tops up by a given amount' do
-    card = Oystercard.new
-    # card.top_up(10)
-    expect(card.top_up(10)).to eq card.balance
-  end
 
-  it 'throws error if balance exceeds 90' do
-    card = Oystercard.new
-    expect{ card.top_up(91) }.to raise_error("Balance cannot exceed 90")
->>>>>>> 4ad3e6c3d66d6fa9fe49917fd53a452b13065e5e
   end
 
 end
