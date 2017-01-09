4f08ddba40f16b9ec914cd4e6400c4cde1b61638
Sam Mckay <sammckay12@gmail.com>
2017-01-09 18:15:52 +0000

    Maximum balance error refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,15 @@
 class Oystercard
 
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
-
-
-
-
+  def top_up(amt)
+    raise 'Balance cannot exceed 90' if (balance + amt) > MAX_BALANCE
+    @balance += amt
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,16 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it 'tops up by a given amount' do
+    card = Oystercard.new
+    # card.top_up(10)
+    expect(card.top_up(10)).to eq card.balance
+  end
+
+  it 'throws error if balance exceeds 90' do
+    card = Oystercard.new
+    maximum_bal = Oystercard::MAX_BALANCE
+    expect{ card.top_up(91) }.to raise_error("Balance cannot exceed #{maximum_bal}")
+  end
 
 end
