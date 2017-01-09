60884039ea512f4cf7e09f49bdef69960c05c4a5
Sam Mckay <sammckay12@gmail.com>
2017-01-09 16:19:09 +0000

    Oystercard initialises with balance of 0

---
+++ lib/oystercard.rb
@@ -0,0 +1,14 @@
+class Oystercard
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+
+
+
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it 'initialises with a balance of 0' do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
+
+
 end
