efbb962254167c3191c67793c9f08e37abd67478
Ben Kielty <bwk103@gmail.com>
2017-01-13 16:32:59 +0000

    Added first version of zone_calc to journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,6 @@
 class Journey
 
-  attr_reader :entry_station, :exit_station, :full_journey, :fare
+  attr_reader :entry_station, :exit_station, :full_journey, :fare, :entry_zone, :exit_zone
 
     MIN_FARE = 1
     PENALTY_CHARGE = 6
@@ -8,6 +8,8 @@ class Journey
   def initialize(entry_station = nil)
     @entry_station = entry_station
     @exit_station = nil
+    @entry_zone = nil
+    @exit_zone = nil
     start_journey
   end
 
@@ -17,15 +19,24 @@ class Journey
 
   def start_journey
     @entry_station = entry_station
+    @entry_zone = entry_station.zone
     @full_journey = { entry: @entry_station, exit: @exit_station }
   end
 
   def complete_journey(exit_station)
     @exit_station = exit_station
+    @exit_zone = exit_station.zone
     @full_journey = { entry: @entry_station, exit: @exit_station }
   end
 
   def fare
-     (@exit_station != nil && @entry_station != nil) ? MIN_FARE : PENALTY_CHARGE
+     (@exit_station != nil && @entry_station != nil) ? MIN_FARE + zone_calc : PENALTY_CHARGE
   end
+
+private
+
+def zone_calc
+  (@entry_zone - @exit_zone).abs
+end
+
 end
